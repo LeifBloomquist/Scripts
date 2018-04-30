219e64d4177e1e305194944ddd9253399b504017
Leif Bloomquist <github@leifbloomquist.net>
2018-04-30 16:10:24 -0400

    Update tether.sh

--- tether.sh
+++ tether.sh
@@ -1,5 +1,9 @@
-ifconfig usb0 up
-dhcpcd -k -d usb0
-dhcpcd -n -d usb0
-route add -host 208.79.218.201 gw 192.168.42.129 dev usb0
+#!/bin/bash
+
+ETH=ens35u1
+
+ifconfig $ETH up
+dhcpcd -k -d $ETH
+dhcpcd -n -d $ETH
+route add -host 208.79.218.201 gw 192.168.42.129 dev $ETH
 route
